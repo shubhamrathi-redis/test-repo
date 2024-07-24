804d486be44980ff247df15c4711c7fd84423b8a
Shubham Rathi <shubham.rathi@redis.com>
2024-07-24 14:14:39 +0530

    update

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# Create the /tmp directory if it does not exist
+mkdir -p /tmp
+
 # Check if the environment variable 're_fqdns' is set
 if [ -z "$re_fqdns" ]; then
-  echo "Environment variable 're_fqdns' is not set." > /temp/test.txt
+  echo "Environment variable 're_fqdns' is not set." > /tmp/test.txt
   echo "Environment variable 're_fqdns' is not set."
   exit 1
 fi
 
-# Create the /temp directory if it does not exist
-mkdir -p /temp
+# Write the value of 're_fqdns' to /tmp/test.txt
+echo "$re_fqdns" > /tmp/test.txt
 
-# Write the value of 're_fqdns' to /temp/test.txt
-echo "$re_fqdns" > /temp/test.txt
-
-echo "Value of 're_fqdns' written to /temp/test.txt"
+echo "Value of 're_fqdns' written to /tmp/test.txt"
